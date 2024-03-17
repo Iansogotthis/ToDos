6d3a04e7ef498d20b3d0db1e97deaefc425fbcef
Ianssogothis <mount88ian@gmail.com>
2024-03-17 01:46:32 -0700

    todolist b4 new style dropD

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -1,9 +1,9 @@
 import { ToDoListItem } from "../ToDoListItem/ToDoListItem";
 
 export default function ToDoList({ todos }) {
+  const steps = ["practice", "project", "prioritize"];
   const toDoListItems = todos.map(function(todo, index) {
-    
-    return <ToDoListItem key={index} todo={ `${index  + 1}. ${todo}`} />;
+    return <ToDoListItem key={index} todo={ `${index  + 1}. ${todo}`} steps={steps} />;
   });
 
   return (
--- src/ToDoListItem/ToDoListItem.jsx
+++ src/ToDoListItem/ToDoListItem.jsx
@@ -1,13 +1,41 @@
-export function ToDoListItem({ todo }){
-  return(
-  <>
-  <div>
-    <ul>
-     <li className = "todo">{todo}</li>
-   </ul>
-   </div>
-   </>
-   )
+import { useState } from "react";
+import styled from "@emotion/styled";
+
+const Dropdown = styled.div`
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  padding: 12px 16px;
+  z-index: 1;
+`;
+
+const ListItem = styled.li`
+  position: relative;
+`;
+
+export function ToDoListItem({ todo, steps }) {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <ListItem className ="Ddown"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {todo}
+      {hover && (
+        <Dropdown>
+          {steps.map(function (step, index) {
+            return (
+              <p className = "pstep" key={index}>
+                Step {index + 1} for {todo}: {step}
+              </p>
+            );
+          })}
+        </Dropdown>
+      )}
+    </ListItem>
+  );
 }
 
 export default ToDoListItem;
